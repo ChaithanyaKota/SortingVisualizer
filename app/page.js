55f581fc6c5ea3534dcc14b4b1a4f9ab7ef934e3
ChaithanyaKota <chaithanya.kota007@gmail.com>
2023-10-22 19:17:55 -0500

    fixed hydration error

--- app/page.js
+++ app/page.js
@@ -1,22 +1,30 @@
 "use client";
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function SortingVisualizer() {
 
-  const [array, setArray] = useState(Array.from({ length: 100 }, () => Math.floor(Math.random() * 1001 + 10)));
+  const [array, setArray] = useState([]);
 
+  useEffect(() => {
+    setArray(Array.from({ length: 100 }, () => Math.floor(Math.random() * 1001 + 10)));
+  }, []);
+  
   function resetArray() {
     setArray(Array.from({ length: 100 }, () => Math.floor(Math.random() * 1001 + 10)));
   }
 
+
   return (
-    <main>
+    <>
       <button className="font-bold" onClick={resetArray}> reset </button>
-        {array.map((value, idx) => (
-          <div className="" key={idx}>{value}</div>
+
+      {array.map((value, idx) => (
+          <div className="" key = {idx}>
+            {value}
+          </div>
         ))}
         
-    </main>
+    </>
   );
 
 }
