75b2b435823c53fc2b4012b9660616740875710d
ChaithanyaKota <chaithanya.kota007@gmail.com>
2023-11-13 17:49:29 -0600

    Added Heap Sort Algorithm

--- app/page.js
+++ app/page.js
@@ -195,10 +195,63 @@ export default function SortingVisualizer() {
     return i + 1;
   }
 
+  // Heap Sort
+  async function heapSort(arr) {
+    const n = arr.length;
+
+    // Build heap
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+      await heapify(arr, n, i);
+    }
+
+    // One by one extract an element from heap
+    for (let i = n - 1; i >= 0; i--) {
+      // Move current root to end
+      const temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+
+      // call max heapify on the reduced heap
+      await heapify(arr, i, 0);
+
+      // Visualize the sorting
+      setArray([...arr]);
+
+      // Wait for some time before next iteration
+      await new Promise((resolve) => setTimeout(resolve, 50));
+    }
+  }
+
+  async function heapify(arr, n, i) {
+    let largest = i; // Initialize largest as root
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest]) {
+      largest = left;
+    }
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest]) {
+      largest = right;
+    }
+
+    // If largest is not root
+    if (largest !== i) {
+      const swap = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = swap;
+
+      // Recursively heapify the affected sub-tree
+      await heapify(arr, n, largest);
+    }
+  }
 
   // ------------------------------------------------RENDERING ARRAY------------------------------------------------ //
   return (
     <>
+      {/* Navbar */}
       <div className="flex justify-between mx-40 my-5">
         <button className="font-bold" onClick={resetArray}>
           reset
@@ -224,7 +277,13 @@ export default function SortingVisualizer() {
         >
           merge Sort
         </button>
+
+        <button className="font-bold" onClick={() => heapSort(array)}>
+          Heap Sort
+        </button>
       </div>
+
+      {/* Array Bars */}
 
       <div className="flex justify-between items-end px-2">
         {array.map((value, idx) => (
